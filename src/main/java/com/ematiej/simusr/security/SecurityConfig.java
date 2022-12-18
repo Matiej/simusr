3a2c189af0e74c81f76d7d53f0d7be49dc0d784d
Maciej Wójcik <maciek.wojcik179@gmail.com>
2022-12-18 14:25:15 +0100

    add swagger configuration file

--- src/main/java/com/ematiej/simusr/security/SecurityConfig.java
+++ src/main/java/com/ematiej/simusr/security/SecurityConfig.java
@@ -3,11 +3,14 @@ package com.ematiej.simusr.security;
 import com.ematiej.simusr.security.jwt.JwtTokenFilter;
 import com.ematiej.simusr.user.database.UserRepository;
 import com.ematiej.simusr.user.domain.UserEntity;
+import com.ematiej.simusr.user.domain.UserRole;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -19,24 +22,53 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final UserRepository repository;
     private final JwtTokenFilter jwtTokenFilter;
 
+    private final static String[] AUTH_DOC_SWAGGER_PATTERNS = {
+            "/swagger-ui/**",
+            "/swaggeradmin-openapi/**",
+            "/v3/api-docs",
+            "/v2/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/configuration/**",
+            "/swagger-resources/configuration/ui",
+            "/swagger-resources/configuration/security",
+            "/swagger-ui.html",
+            "/swagger-ui.html/**",
+            "/swagger-ui.html#/**",
+            "/swagger-ui.html#/*",
+            "/swagger-ui/**",
+            "/swagger-ui.html#/swagger-welcome-controller/**",
+            "/webjars/springfox-swagger-ui/**",
+            "/webjars/**",
+    };
+
+    private final static String[] POST_AUTH_ALL_USERS_PATTERNS = {
+            "/users",
+            "/users/**"
+    };
+
     //todo do wywalenia
     @EventListener(ApplicationReadyEvent.class)
     public void saveUser() {
-        UserEntity user = new UserEntity("maciek@maciek.pl", getBcryptPasswordEncoder().encode("pass123"));
-        user.setRoles("ROLE_USER");
-        repository.save(user);
+        UserEntity user1 = new UserEntity("maciek@maciek.pl", getBcryptPasswordEncoder().encode("pass123"), Set.of(UserRole.ADMIN));
+        UserEntity user2 = new UserEntity("secretary@maciek.pl", getBcryptPasswordEncoder().encode("pass123"), Set.of(UserRole.USER));
+        List<UserEntity> userEntities = repository.saveAll(Set.of(user1, user2));
+        log.info("Users has been saved successful: " + userEntities.stream().map(UserEntity::getUsername).collect(Collectors.joining(" ; ")));
     }
-
-//    @Bean
-//    public AuthenticationManager authorizationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-//        return authenticationConfiguration.getAuthenticationManager();
-//    }
 
     @Bean
     public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) {
@@ -56,7 +88,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
-                .mvcMatchers("/users/login").permitAll()
+                .mvcMatchers(HttpMethod.POST, POST_AUTH_ALL_USERS_PATTERNS).permitAll()
+                .mvcMatchers(AUTH_DOC_SWAGGER_PATTERNS).permitAll()
                 .anyRequest().authenticated()
                 .and().addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
